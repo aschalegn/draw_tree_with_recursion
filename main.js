1c3b17fe8e2f66da74bf4199f40b082c2a60913d
aschaley ascheneke <schaley1@gmail.com>
2021-03-08 12:14:57 +0200

    add direcion to branch

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const canvas = document.querySelector("#canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
-let lastX = canvas.width / 2;
-let lastY = 0;
+
 let totalBranches = 0;
+
 class Branch {
     constructor(x, y, width, height, parent, branchess) {
         this.x = x
@@ -12,18 +12,31 @@ class Branch {
         this.width = width
         this.height = height
         this.parent = parent;
-        this.branchess = branchess
+        this.branchess = branchess;
+        this.direction = this.setDirection();
+    }
+
+    setDirection() {
+        const d = Math.random();
+        if (d < 0.5) {
+            return "left"
+        }
+        if (d > 0.5) {
+            return "right"
+        }
     }
 }
 
 class Tree {
-    constructor(x, y, width, height, branchess) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
+    constructor(x, y, width, height, totalBranches) {
+        this.x = x;
+        this.y = y;
+        this.root = null;
         this.parent = null;
-        this.branchess = branchess
+        this.width = width;
+        this.height = height;
+        this.branchess = [];
+        this.totalBranches = totalBranches;
     }
 
     creat() {
@@ -31,68 +44,71 @@ class Tree {
     }
 
     drawTree = (tree) => {
-        if (!tree) return;
+        if (!tree.branchess) { console.log("kjhg"); }
+        if (!tree) {
+            console.log("no tree");
+            return
+        };
 
-        if (tree.branchess) {
-            for (let i = 0; i < tree.branchess.length; i++) {
-                {
-                    ctx.fillStyle = "orange";
-                    ctx.beginPath();
-                    if (!tree.parent) {
-                        ctx.lineWidth = 15
-                    }
-                    else { ctx.lineWidth = 5 };
-                    ctx.moveTo(tree.x, tree.y);
-                    ctx.lineTo(tree.x + tree.width, tree.y - tree.height);
-                    ctx.quadraticCurveTo(tree.x, tree.y, tree.x + tree.width, tree.y - tree.height);
-                    ctx.strokeStyle = "orange";
-                    ctx.stroke();
-                    const branch = tree.branchess[i];
-                    if (!branch) {
-                        for (let i = 0; i < 5; i++) {
-                            ctx.beginPath();
-                            ctx.arc(tree.x + tree.width, tree.y - tree.height, 50, 0, Math.PI * 2);
-                            ctx.stroke();
-                        }
-                    }
-                    this.drawTree(branch);
-                }
+        // if (tree.branchess) {
+        for (let i = 0; i < tree.branchess.length; i++) {
+            const branch = tree.branchess[i];
+            const color = this.setColor();
+            ctx.beginPath();
+            ctx.fillStyle = `rgb(${color.r},${color.g},${color.b})`;
+            if (!tree.parent) {
+                ctx.lineWidth = 10;
+                // ctx.moveTo(branch.x + tree.width, branch.y - tree.height);
+                // ctx.quadraticCurveTo(branch.x - 30, branch.y, branch.x + branch.width, branch.y - branch.height);
             }
+            else {
+                ctx.lineWidth = 3
+            };
+            const rightOrLeft = branch.direction === "left" ? branch.x - branch.width : branch.x + branch.width;
+            ctx.moveTo(branch.x, branch.y);
+            ctx.lineTo(rightOrLeft, branch.y - branch.height);
+            // ctx.quadraticCurveTo(branch.x - 30, branch.y, rightOrLeft, branch.y - branch.height);
+            ctx.strokeStyle = `rgb(${color.r},${color.g},${color.b})`;
+            ctx.stroke();
+            // if (i === tree.branchess.length - 1) {
+            //     for (let i = 0; i < 5; i++) {
+            //         ctx.beginPath();
+            //         ctx.arc(rightOrLeft, tree.y - tree.height, 10, 0, Math.PI * 2);
+            //         ctx.stroke();
+            //     }
+            // }
+            this.drawTree(branch);
         }
+    }
+
+    setColor() {
+        const r = Math.random() * 255;
+        const g = Math.random() * 255;
+        const b = Math.random() * 255;
+        return { r, g, b };
     }
 
     addBranchess = (tree, n) => {
-        if (!tree || totalBranches >= 10) return;
+        if (totalBranches >= 15) return;
 
         for (let i = 0; i < n; i++) {
-            const width = Math.floor(Math.random() * 50);
-            const height = Math.floor(Math.random() * 50);
-            
+            const width = Math.floor(Math.random() * 100) + 10;
+            const height = Math.floor(Math.random() * 100) + 10;
             const randomChild = Math.floor(Math.random() * 5);
-            console.log(randomChild);
-            const branch = new Branch(tree.x + tree.width, tree.y - tree.height, width, height, tree, []);
+            const rightOrLeft = tree.direction === "left" ? tree.x - tree.width : tree.x + tree.width;
+            // x=100, width= 30 => rightOrLeft = 70
+            const branch = new Branch(rightOrLeft, tree.y - tree.height, width, height, tree, []);
             tree.branchess.push(branch);
             totalBranches += 1;
-
             this.addBranchess(branch, randomChild);
         }
     }
-
-    // countBranchess = (tree, sum) => {
-    //     for (let i = 0; i < tree.branchess.length; i++) {
-    //         const branch = tree.branchess[i];
-    //         sum += 1;
-    //         this.countBranchess(branch, sum);
-    //     }
-    //     return sum;
-    // }
 
     draw() {
         this.drawTree(this);
     }
 }
 
-const tree1 = new Tree(0, canvas.height - 100, 50, 50, []);
-tree1.creat();
-tree1.draw();
-
+const tree = new Tree(canvas.width / 2, canvas.height, 50, 50);
+tree.creat();
+tree.draw();
